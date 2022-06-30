09139616cb0607ce82a14a44cb37942663f79ca7
JongeunPark <abepje@kaist.ac.kr>
2022-06-30 15:40:40 +0900

    Tab and Fragment Added...

--- app/src/main/java/com/example/project_1/MainActivity.java
+++ app/src/main/java/com/example/project_1/MainActivity.java
@@ -1,14 +1,124 @@
 package com.example.project_1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+    private DrawerLayout drawerLayout;
+    private View drawerView;
+
+    Button btn1, btn2, btn3;
+//    Button btn_home;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerView = (View) findViewById(R.id.drawer);
+
+        Button btn_open = (Button) findViewById(R.id.btn_open);
+        btn_open.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(drawerView);
+
+
+            }
+        });
+
+        Button btn_close = (Button) findViewById(R.id.btn_close);
+        btn_close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.closeDrawers();
+            }
+        });
+
+        drawerLayout.setDrawerListener(listener);
+        drawerView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                return true;
+            }
+        });
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        Fragment_Address fragmentAddress = new Fragment_Address();
+        transaction.replace(R.id.frame, fragmentAddress);
+        transaction.commit();
+
+
+        btn1 = (Button) findViewById(R.id.btn_1);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                Fragment_Address fragmentAddress = new Fragment_Address();
+                transaction.replace(R.id.frame, fragmentAddress);
+                transaction.commit();
+            }
+        });
+
+        btn2 = (Button) findViewById(R.id.btn_2);
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                Fragment_Gallery fragmentGallery = new Fragment_Gallery();
+                transaction.replace(R.id.frame, fragmentGallery);
+                transaction.commit();
+            }
+        });
+
+        btn3 = (Button) findViewById(R.id.btn_3);
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                Fragment_Free fragmentFree = new Fragment_Free();
+                transaction.replace(R.id.frame, fragmentFree);
+                transaction.commit();
+            }
+        });
+
+
     }
+
+    DrawerLayout.DrawerListener listener = new DrawerLayout.DrawerListener() {
+        @Override
+        public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
+
+        }
+
+        @Override
+        public void onDrawerOpened(@NonNull View drawerView) {
+
+        }
+
+        @Override
+        public void onDrawerClosed(@NonNull View drawerView) {
+
+        }
+
+        @Override
+        public void onDrawerStateChanged(int newState) {
+
+        }
+    };
 }
